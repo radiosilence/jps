d901473246a7cc2a4acd807e6e39555ef3175313
James Cleveland <jamescleveland@gmail.com>
2009-09-20 17:19:01 +0100

    Added index yo

---
+++ _wwwroot/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Das index, ja?
+ */
+ 
+$title = "Jonathan Paul Sharp | Property Services";
+?><!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $title ?>
+	</head>
+	<body>
+		<h1><?php echo $title ?></h1>
+	</body>
+</html>
